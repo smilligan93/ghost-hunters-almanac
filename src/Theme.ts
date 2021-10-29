e1f39a69a94c952306aa1d34b8df120a6b043b19
Shawn Milligan <smilligan93@gmail.com>
2021-10-29 12:08:22 -0400

    color tabs to match the rest of the app

--- src/Theme.ts
+++ src/Theme.ts
@@ -7,5 +7,12 @@ export const Theme = {
             'accent-2': '#ab6d23',
             'accent-3': '#51050f',
         }
+    },
+    tab: {
+        active: {
+            color: 'accent-1',
+        },
+        color: 'light-1',
+        border: { color: 'light-1', active: { color: 'accent-1' } }
     }
 }
